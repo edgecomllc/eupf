2e893a1caea573b6c4a2a13a1a24cadd9d256d1b
Mark Nefedov <mark.nefedov17@gmail.com>
2023-02-10 17:23:57 +0300

    mount debugfs for "in container" tracing.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,12 @@
 #!/bin/env sh
 
-# Mount bpffs if not shared already
+# Mount bpffs and debugfs if not present already
 if [[ $(/bin/mount | /bin/grep /sys/fs/bpf -c) -eq 0 ]]; then
     /bin/mount bpffs /sys/fs/bpf -t bpf;
 fi
+if [[ $(/bin/mount | /bin/grep /sys/kernel/debug -c) -eq 0 ]]; then
+    /bin/mount debugfs /sys/kernel/debug -t debugfs;
+fi
+
 # Run app replacing current shell (to preserve signal handling) and forward cmd arguments
 exec /app/bin/eupf "$@"
