a6ece73d9056d5bef7908760ba92878c949eb656
Mark Nefedov <mark.nefedov17@gmail.com>
2023-02-16 12:41:18 +0000

    Fix indentation

--- scripts/scapy/pfcp_sim.py
+++ scripts/scapy/pfcp_sim.py
@@ -5,12 +5,12 @@ from scapy.contrib.pfcp import *
 
 # TODO: Understand why packet only visible in tcpdump but not reacing software
 
+
 def test_hearbeat():
     pfcp_heartbeat = Ether() / IP(dst="127.0.0.1") / \
-        UDP(dport=8805) / PFCP() /
-             PFCPHeartbeatRequest(IE_list=[
-                 IE_RecoveryTimeStamp()
-             ])
+        UDP(dport=8805) / PFCP() / PFCPHeartbeatRequest(IE_list=[
+            IE_RecoveryTimeStamp()
+        ])
     print(pfcp_heartbeat.show())
     ans = srp1(pfcp_heartbeat, inter=1,  timeout=5, iface='lo')
     print(ans)
