40072ed774cecdda8fa66e09a6759b1e02bdb955
Mark Nefedov <mark.nefedov17@gmail.com>
2023-02-18 15:47:25 +0000

    sane variable names.
    api refacto & new context map api
    
    Revert "api refacto & new context map api"
    
    This reverts commit f3adc87ee28ceb8ccb14d11a0236e1fdfa50598f.

--- cmd/eupf/pfcp_connection.go
+++ cmd/eupf/pfcp_connection.go
@@ -27,27 +27,27 @@ func CreatePfcpConnection(addr string, pfcpHandlerMap PfcpHanderMap) (*PfcpConne
 	}, nil
 }
 
-func (c *PfcpConnection) Run() {
+func (connection *PfcpConnection) Run() {
 	buf := make([]byte, 1500)
 	for {
-		n, _, err := c.udpConn.ReadFromUDP(buf)
+		n, _, err := connection.udpConn.ReadFromUDP(buf)
 		if err != nil {
 			log.Printf("Error reading from UDP socket: %s", err)
 			continue
 		}
-		log.Printf("Received %d bytes from %s", n, c.udpConn.RemoteAddr())
-		c.pfcpHandlerMap.Handle(c, buf[:n])
+		log.Printf("Received %d bytes from %s", n, connection.udpConn.RemoteAddr())
+		connection.pfcpHandlerMap.Handle(connection, buf[:n])
 	}
 }
 
-func (c *PfcpConnection) Close() {
-	c.udpConn.Close()
+func (connection *PfcpConnection) Close() {
+	connection.udpConn.Close()
 }
 
-func (c *PfcpConnection) Send(b []byte) (int, error) {
-	return c.udpConn.WriteToUDP(b, c.RemoteAddr())
+func (connection *PfcpConnection) Send(b []byte) (int, error) {
+	return connection.udpConn.WriteToUDP(b, connection.RemoteAddr())
 }
 
-func (c *PfcpConnection) RemoteAddr() *net.UDPAddr {
-	return c.udpConn.RemoteAddr().(*net.UDPAddr)
+func (connection *PfcpConnection) RemoteAddr() *net.UDPAddr {
+	return connection.udpConn.RemoteAddr().(*net.UDPAddr)
 }
--- cmd/eupf/pfcp_handlers.go
+++ cmd/eupf/pfcp_handlers.go
@@ -12,34 +12,34 @@ type PfcpFunc func(conn *PfcpConnection, msg message.Message) error
 
 type PfcpHanderMap map[uint8]PfcpFunc
 
-func (h PfcpHanderMap) Handle(c *PfcpConnection, buf []byte) error {
-	log.Printf("Handling PFCP message from %s", c.RemoteAddr())
+func (handlerMap PfcpHanderMap) Handle(conn *PfcpConnection, buf []byte) error {
+	log.Printf("Handling PFCP message from %s", conn.RemoteAddr())
 	msg, err := message.Parse(buf)
 	if err != nil {
 		log.Printf("Ignored undecodable message: %x, error: %s", buf, err)
 		return err
 	}
 	log.Printf("Parsed PFCP message: %s", msg)
-	if handler, ok := h[msg.MessageType()]; ok {
-		err := handler(c, msg)
+	if handler, ok := handlerMap[msg.MessageType()]; ok {
+		err := handler(conn, msg)
 		if err != nil {
 			log.Printf("Error handling PFCP message: %s", err)
 			return err
 		}
 	} else {
-		log.Printf("Got unexpected message %s: %s, from: %s", msg.MessageTypeName(), msg, c.RemoteAddr())
+		log.Printf("Got unexpected message %s: %s, from: %s", msg.MessageTypeName(), msg, conn.RemoteAddr())
 	}
 	return nil
 }
 
-func handlePfcpHeartbeatRequest(c *PfcpConnection, msg message.Message) error {
+func handlePfcpHeartbeatRequest(conn *PfcpConnection, msg message.Message) error {
 	hbreq := msg.(*message.HeartbeatRequest)
 	ts, err := hbreq.RecoveryTimeStamp.RecoveryTimeStamp()
 	if err != nil {
-		log.Printf("Got Heartbeat Request with invalid TS: %s, from: %s", err, c.RemoteAddr())
+		log.Printf("Got Heartbeat Request with invalid TS: %s, from: %s", err, conn.RemoteAddr())
 		return err
 	} else {
-		log.Printf("Got Heartbeat Request with TS: %s, from: %s", ts, c.RemoteAddr())
+		log.Printf("Got Heartbeat Request with TS: %s, from: %s", ts, conn.RemoteAddr())
 	}
 
 	// #TODO: add sequence tracking for individual sessions
@@ -50,17 +50,17 @@ func handlePfcpHeartbeatRequest(c *PfcpConnection, msg message.Message) error {
 		return err
 	}
 
-	if _, err := c.Send(hbres); err != nil {
+	if _, err := conn.Send(hbres); err != nil {
 		log.Print(err)
 		return err
 	}
-	log.Printf("Sent Heartbeat Response to: %s", c.RemoteAddr())
+	log.Printf("Sent Heartbeat Response to: %s", conn.RemoteAddr())
 	return nil
 }
 
-func handlePfcpAssociationSetupRequest(c *PfcpConnection, msg message.Message) error {
+func handlePfcpAssociationSetupRequest(conn *PfcpConnection, msg message.Message) error {
 	asreq := msg.(*message.AssociationSetupRequest)
 	log.Print(asreq)
-	log.Printf("Got Association Setup Request from: %s", c.RemoteAddr())
+	log.Printf("Got Association Setup Request from: %s", conn.RemoteAddr())
 	return nil
 }
