ea0fe75ad4bdf4eed280896727a157f4cde0f31b
Mark Nefedov <mark.nefedov17@gmail.com>
2023-03-17 19:32:59 +0000

    Add dto

--- cmd/eupf/api.go
+++ cmd/eupf/api.go
@@ -5,6 +5,8 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/prometheus/client_golang/prometheus"
+	dto "github.com/prometheus/client_model/go"
 )
 
 type ApiServer struct {
@@ -35,10 +37,35 @@ func CreateApiServer(bpfObjects *BpfObjects, pfcp_srv *PfcpConnection) *ApiServe
 	router.GET("/config", func(c *gin.Context) {
 		c.IndentedJSON(http.StatusOK, config)
 	})
-
+	router.GET("/xdp_stats", func(c *gin.Context) {
+		type XdpStats struct {
+			Aborted  uint32 `json:"aborted"`
+			Drop     uint32 `json:"drop"`
+			Pass     uint32 `json:"pass"`
+			Tx       uint32 `json:"tx"`
+			Redirect uint32 `json:"redirect"`
+		}
+		// Fill from existing prometheus metrics
+		xdpStats := XdpStats{
+			Aborted:  uint32(getMetricValue(UpfXdpAborted)),
+			Drop:     uint32(getMetricValue(UpfXdpDrop)),
+			Pass:     uint32(getMetricValue(UpfXdpPass)),
+			Tx:       uint32(getMetricValue(UpfXdpTx)),
+			Redirect: uint32(getMetricValue(UpfXdpRedirect)),
+		}
+		c.IndentedJSON(http.StatusOK, xdpStats)
+	})
 	return &ApiServer{router: router}
 }
 
 func (server *ApiServer) Run(addr string) {
 	server.router.Run(addr)
+}
+
+func getMetricValue(col prometheus.Collector) float64 {
+	c := make(chan prometheus.Metric, 1) // 1 for metric with no vector
+	col.Collect(c)                       // collect current metric value into the channel
+	m := dto.Metric{}
+	_ = (<-c).Write(&m) // read metric value from the channel
+	return *m.Counter.Value
 }
