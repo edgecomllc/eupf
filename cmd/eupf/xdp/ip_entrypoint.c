1ae463b6cd1050a49d06633835053f618ca65f38
arozhkevich <aleksandr.rozhkevich@adm-systems.com>
2023-04-20 10:31:01 +0300

    add direct lookup flag in fib_lookup

--- cmd/eupf/xdp/ip_entrypoint.c
+++ cmd/eupf/xdp/ip_entrypoint.c
@@ -20,7 +20,6 @@
 #include "xdp/pdr.h"
 
 #undef bpf_printk
-//#define bpf_printk(fmt, ...)
 #define bpf_printk(fmt, ...)                       \
     ({                                             \
         static const char ____fmt[] = fmt;         \
@@ -199,7 +198,7 @@ static __always_inline __u32 route_ipv4(struct xdp_md *ctx, struct ethhdr *eth, 
     fib_params.ipv4_dst = ip4->daddr;
     fib_params.ifindex = ctx->ingress_ifindex;
 
-    int rc = bpf_fib_lookup(ctx, &fib_params, sizeof(fib_params), BPF_FIB_LOOKUP_OUTPUT);
+    int rc = bpf_fib_lookup(ctx, &fib_params, sizeof(fib_params), BPF_FIB_LOOKUP_OUTPUT | BPF_FIB_LOOKUP_DIRECT);
     bpf_printk("upf: bpf_fib_lookup %d: %pI4 -> %pI4", rc, &ip4->saddr, &ip4->daddr);
     bpf_printk("upf: bpf_fib_lookup nexthop: %pI4", &fib_params.ipv4_dst);
 
@@ -265,32 +264,6 @@ static __always_inline __u32 handle_echo_request(struct packet_context *ctx, str
     return XDP_TX;
 }
 
-static __always_inline __u32 limit_rate_sliding_window(struct xdp_md *ctx, __u64* windows_start, const __u64 rate)
-{
-	void *data = (void *)(long)ctx->data;
-	void *data_end = (void *)(long)ctx->data_end;
-
-	static const __u64 NSEC_PER_SEC = 1000000000ULL;
-    static const __u64 window_size = 5000000ULL;
-	__u64 tx_time = (data_end - data) * 8 * NSEC_PER_SEC / rate;
-	__u64 now = bpf_ktime_get_ns();
-
-	__u64 start = *(volatile __u64 *)windows_start;
-	if (start + tx_time > now)
-		return XDP_DROP;
-
-	if (start + window_size < now)
-	{
-		*(volatile __u64 *)&windows_start = now - window_size + tx_time;
-		return XDP_PASS;
-	}
-
-	*(volatile __u64 *)&windows_start = start + tx_time;
-	//__sync_fetch_and_add(&window->start, tx_time);
-	return XDP_PASS;
-}
-
-
 static __always_inline __u32 handle_core_packet_ipv4(struct xdp_md *ctx, const struct iphdr *ip4)
 {
     struct pdr_info* pdr = bpf_map_lookup_elem(&pdr_map_downlink_ip4, &ip4->daddr);
@@ -309,20 +282,6 @@ static __always_inline __u32 handle_core_packet_ipv4(struct xdp_md *ctx, const s
 
     //Only forwarding action supported at the moment
     if(!(far->action & FAR_FORW))
-        return XDP_DROP;
-
-    struct qer_info* qer = bpf_map_lookup_elem(&qer_map, &pdr->qer_id);
-    if(!qer) {
-        bpf_printk("upf: no downlink session qer for ip:%pI4 qer:%d", &ip4->daddr, pdr->qer_id);
-            return XDP_DROP;
-    }
-
-    bpf_printk("upf: qer:%d gate_status:%d mbr:%d", pdr->qer_id, qer->dl_gate_status, qer->dl_maximum_bitrate);
-
-    if(qer->dl_gate_status != GATE_STATUS_OPEN)
-        return XDP_DROP;
-
-    if(XDP_DROP == limit_rate_sliding_window(ctx, &qer->dl_start, qer->dl_maximum_bitrate))
         return XDP_DROP;
 
     bpf_printk("upf: use mapping %pI4 -> TEID:%d", &ip4->daddr, far->teid);
@@ -454,24 +413,10 @@ static __always_inline __u32 handle_access_packet(struct packet_context *ctx, __
             return XDP_DROP;
     }
 
-    bpf_printk("upf: far:%d action:%d outer_header_creation:%d", pdr->far_id, far->action, far->outer_header_creation);
+    bpf_printk("upf: far for teid:%d far:%d action:%d", teid, pdr->far_id, far->action);
 
     //Only forwarding action supported at the moment
     if(!(far->action & FAR_FORW))
-        return XDP_DROP;
-
-    struct qer_info* qer = bpf_map_lookup_elem(&qer_map, &pdr->qer_id);
-    if(!qer) {
-        bpf_printk("upf: no uplink session qer for teid:%d qer:%d", teid, pdr->qer_id);
-            return XDP_DROP;
-    }
-
-    bpf_printk("upf: qer:%d gate_status:%d mbr:%d", pdr->qer_id, qer->ul_gate_status, qer->ul_maximum_bitrate);
-
-    if(qer->ul_gate_status != GATE_STATUS_OPEN)
-        return XDP_DROP;
-
-    if(XDP_DROP == limit_rate_sliding_window(ctx->ctx, &qer->ul_start, qer->ul_maximum_bitrate))
         return XDP_DROP;
 
     if(pdr->outer_header_removal == OHR_GTP_U_UDP_IPv4) 
@@ -678,7 +623,7 @@ static __always_inline __u32 process_packet(struct packet_context *ctx)
 SEC("xdp/upf_ip_entrypoint")
 int upf_ip_entrypoint_func(struct xdp_md *ctx)
 {
-    //bpf_printk("upf_ip_entrypoint start");
+    bpf_printk("upf_ip_entrypoint start");
     void *data_end = (void *)(long)ctx->data_end;
     void *data = (void *)(long)ctx->data;
 
