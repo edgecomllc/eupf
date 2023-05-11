cb35be18b0526310120fd31d3462dfaaddb002a0
Mark Nefedov <mark.nefedov17@gmail.com>
2023-05-12 00:41:38 +0300

    Use query parameters.

--- cmd/eupf/api.go
+++ cmd/eupf/api.go
@@ -36,8 +36,8 @@ func CreateApiServer(bpfObjects *BpfObjects, pfcpSrv *PfcpConnection, forwardPla
 		}
 		sessions := v1.Group("/pfcp_sessions")
 		{
-			sessions.GET("", ListPfcpSessions(pfcpSrv))
-			sessions.GET(":arg", ListPfcpSessionsFiltered(pfcpSrv))
+			//sessions.GET("", ListPfcpSessions(pfcpSrv))
+			sessions.GET("", ListPfcpSessionsFiltered(pfcpSrv))
 		}
 		v1.GET("/config", DisplayConfig())
 		v1.GET("/xdp_stats", DisplayXdpStatistics(forwardPlaneStats))
@@ -93,7 +93,7 @@ func DisplayConfig() func(c *gin.Context) {
 // @Tags PFCP
 // @Produce  json
 // @Success 200 {object} NodeAssociationMap
-// @Router /associations/full [get]
+// @Router /pfcp_associations/full [get]
 func ListPfcpAssociationsFull(pfcpSrv *PfcpConnection) func(c *gin.Context) {
 	return func(c *gin.Context) {
 		c.IndentedJSON(http.StatusOK, pfcpSrv.nodeAssociations)
@@ -113,7 +113,7 @@ type NodeAssociationMapNoSession map[string]NodeAssociationNoSession
 // @Tags PFCP
 // @Produce  json
 // @Success 200 {object} NodeAssociationMapNoSession
-// @Router /associations [get]
+// @Router /pfcp_associations [get]
 func ListPfcpAssociations(pfcpSrv *PfcpConnection) func(c *gin.Context) {
 	return func(c *gin.Context) {
 		nodeAssociationsNoSession := make(NodeAssociationMapNoSession)
@@ -143,13 +143,13 @@ func GetAllSessions(nodeMap NodeAssociationMap) []Session {
 // @Tags PFCP
 // @Produce  json
 // @Success 200 {object} []Session
-// @Router /sessions [get]
-func ListPfcpSessions(pfcpSrv *PfcpConnection) func(c *gin.Context) {
-	return func(c *gin.Context) {
-		sessions := GetAllSessions(pfcpSrv.nodeAssociations)
-		c.IndentedJSON(http.StatusOK, sessions)
-	}
-}
+// @Router /pfcp_sessions [get]
+//func ListPfcpSessions(pfcpSrv *PfcpConnection) func(c *gin.Context) {
+//	return func(c *gin.Context) {
+//		sessions := GetAllSessions(pfcpSrv.nodeAssociations)
+//		c.IndentedJSON(http.StatusOK, sessions)
+//	}
+//}
 
 func FilterSessionsByIP(sessions []Session, filterByIP net.IP) []Session {
 	var filteredSessions []Session
@@ -206,17 +206,19 @@ func FilterSessionsByTeid(sessions []Session, filterByTeid uint32) []Session {
 // @Summary List PFCP sessions filtered by TEID or IP
 // @Tags PFCP
 // @Produce  json
-// @Param arg path string true "Ip or TEID"
+// @Param ip query string false "ip"
+// @Param teid query int false "teid"
 // @Success 200 {object} []Session
-// @Router /sessions/{arg} [get]
+// @Router /pfcp_sessions [get]
 func ListPfcpSessionsFiltered(pfcpSrv *PfcpConnection) func(c *gin.Context) {
 	return func(c *gin.Context) {
 		sessions := GetAllSessions(pfcpSrv.nodeAssociations)
-		arg := c.Param("arg")
-		if ip := net.ParseIP(arg); ip != nil {
+		sIp := c.Query("ip")
+		if ip := net.ParseIP(sIp); ip != nil {
 			sessions = FilterSessionsByIP(sessions, ip)
 		}
-		if teid, err := strconv.Atoi(arg); err == nil {
+		sTeid := c.Query("teid")
+		if teid, err := strconv.Atoi(sTeid); err == nil {
 			sessions = FilterSessionsByTeid(sessions, uint32(teid))
 		}
 		c.IndentedJSON(http.StatusOK, sessions)
