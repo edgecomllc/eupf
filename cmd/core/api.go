766d0c98f3374a1bcc4fa7b3724f681acf487bc2
Кирилл Дудников <kddudnikov@edu.hse.ru>
2023-09-18 13:20:52 +0300

    feat: add packet statistic api

--- cmd/core/api.go
+++ cmd/core/api.go
@@ -60,16 +60,45 @@ func CreateApiServer(bpfObjects *ebpf.BpfObjects, pfcpSrv *PfcpConnection, forwa
 	return &ApiServer{router: router}
 }
 
+type PacketStats struct {
+	RxArp      uint64 `json:"rx_arp"`
+	RxIcmp     uint64 `json:"rx_icmp"`
+	RxIcmp6    uint64 `json:"rx_icmp6"`
+	RxIp4      uint64 `json:"rx_ip4"`
+	RxIp6      uint64 `json:"rx_ip6"`
+	RxTcp      uint64 `json:"rx_tcp"`
+	RxUdp      uint64 `json:"rx_udp"`
+	RxOther    uint64 `json:"rx_other"`
+	RxGtpEcho  uint64 `json:"rx_gtp_echo"`
+	RxGtpPdu   uint64 `json:"rx_gtp_pdu"`
+	RxGtpOther uint64 `json:"rx_gtp_other"`
+	RxGtpUnexp uint64 `json:"rx_gtp_unexp"`
+}
+
 // DisplayPacketStats godoc
 // @Summary Display packet statistics
 // @Description Display packet statistics
 // @Tags Packet
 // @Produce  json
-// @Success 200 {object} ebpf.UpfCounters
+// @Success 200 {object} PacketStats
 // @Router /packet_stats [get]
 func DisplayPacketStats(forwardPlaneStats ebpf.UpfXdpActionStatistic) func(c *gin.Context) {
 	return func(c *gin.Context) {
-		c.IndentedJSON(http.StatusOK, forwardPlaneStats.GetUpfExtStatField())
+		packets := forwardPlaneStats.GetUpfExtStatField()
+		c.IndentedJSON(http.StatusOK, PacketStats{
+			RxArp:      packets.RxArp,
+			RxIcmp:     packets.RxIcmp,
+			RxIcmp6:    packets.RxIcmp6,
+			RxIp4:      packets.RxIp4,
+			RxIp6:      packets.RxIp6,
+			RxTcp:      packets.RxTcp,
+			RxUdp:      packets.RxUdp,
+			RxOther:    packets.RxOther,
+			RxGtpEcho:  packets.RxGtpEcho,
+			RxGtpPdu:   packets.RxGtpPdu,
+			RxGtpOther: packets.RxGtpOther,
+			RxGtpUnexp: packets.RxGtpUnexp,
+		})
 	}
 }
 
