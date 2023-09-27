10e3fcef285217bb441e7f4cb2f412ca98ca31ae
pirog-spb <arozhkevich@edgecom.ru>
2023-09-28 00:27:48 +0300

    add gtp with pdr benchmark

--- cmd/ebpf/objects_test.go
+++ cmd/ebpf/objects_test.go
@@ -20,6 +20,7 @@ import (
 	"net"
 	"reflect"
 	"testing"
+	"unsafe"
 
 	"github.com/google/gopacket"
 	"github.com/google/gopacket/layers"
@@ -68,6 +69,120 @@ func testArpBenchmark(bpfObjects *BpfObjects, repeat int) (int64, error) {
 	}
 
 	_, duration, err := bpfObjects.UpfIpEntrypointFunc.Benchmark(packetArp.Bytes(), repeat, func() {})
+	if err != nil {
+		return 0, fmt.Errorf("benchmark run failed: %v", err)
+	}
+
+	return duration.Nanoseconds(), nil
+}
+
+func testGtpBenchmark(bpfObjects *BpfObjects, repeat int) (int64, error) {
+
+	packet := gopacket.NewSerializeBuffer()
+	if err := gopacket.SerializeLayers(packet, gopacket.SerializeOptions{},
+		&layers.Ethernet{
+			SrcMAC:       net.HardwareAddr{1, 0, 0, 3, 0, 10},
+			DstMAC:       net.HardwareAddr{1, 0, 0, 3, 0, 20},
+			EthernetType: layers.EthernetTypeIPv4,
+		},
+		&layers.IPv4{
+			Version:  4,
+			DstIP:    net.IP{10, 3, 0, 10},
+			SrcIP:    net.IP{10, 3, 0, 20},
+			Protocol: layers.IPProtocolUDP,
+			IHL:      5,
+		},
+		&layers.UDP{
+			DstPort: 2152,
+			SrcPort: 2152,
+		},
+		&layers.GTPv1U{
+			Version:        1,
+			MessageType:    255, // GTPU_G_PDU
+			TEID:           1,
+			SequenceNumber: 0,
+		},
+		&layers.IPv4{
+			Version:  4,
+			DstIP:    net.IP{1, 1, 1, 1},
+			SrcIP:    net.IP{10, 60, 0, 1},
+			Protocol: layers.IPProtocolICMPv4,
+			IHL:      5,
+		},
+		&layers.ICMPv4{
+			TypeCode: layers.ICMPv4TypeEchoRequest,
+			Id:       0,
+			Seq:      0,
+		},
+	); err != nil {
+		return 0, fmt.Errorf("serializing input packet failed: %v", err)
+	}
+
+	_, duration, err := bpfObjects.UpfIpEntrypointFunc.Benchmark(packet.Bytes(), repeat, func() {})
+	if err != nil {
+		return 0, fmt.Errorf("benchmark run failed: %v", err)
+	}
+
+	return duration.Nanoseconds(), nil
+}
+
+func testGtpWithPDRBenchmark(bpfObjects *BpfObjects, repeat int) (int64, error) {
+
+	teid := uint32(1)
+
+	packet := gopacket.NewSerializeBuffer()
+	if err := gopacket.SerializeLayers(packet, gopacket.SerializeOptions{},
+		&layers.Ethernet{
+			SrcMAC:       net.HardwareAddr{1, 0, 0, 3, 0, 10},
+			DstMAC:       net.HardwareAddr{1, 0, 0, 3, 0, 20},
+			EthernetType: layers.EthernetTypeIPv4,
+		},
+		&layers.IPv4{
+			Version:  4,
+			DstIP:    net.IP{10, 3, 0, 10},
+			SrcIP:    net.IP{10, 3, 0, 20},
+			Protocol: layers.IPProtocolUDP,
+			IHL:      5,
+		},
+		&layers.UDP{
+			DstPort: 2152,
+			SrcPort: 2152,
+		},
+		&layers.GTPv1U{
+			Version:        1,
+			MessageType:    255, // GTPU_G_PDU
+			TEID:           teid,
+			SequenceNumber: 0,
+		},
+		&layers.IPv4{
+			Version:  4,
+			DstIP:    net.IP{1, 1, 1, 1},
+			SrcIP:    net.IP{10, 60, 0, 1},
+			Protocol: layers.IPProtocolICMPv4,
+			IHL:      5,
+		},
+		&layers.ICMPv4{
+			TypeCode: layers.ICMPv4TypeEchoRequest,
+			Id:       0,
+			Seq:      0,
+		},
+	); err != nil {
+		return 0, fmt.Errorf("serializing input packet failed: %v", err)
+	}
+
+	pdr := PdrInfo{OuterHeaderRemoval: 0, FarId: 1, QerId: 1}
+	far := FarInfo{Action: 2, OuterHeaderCreation: 1, RemoteIP: 1, LocalIP: 2, Teid: 2, TransportLevelMarking: 0}
+	qer := QerInfo{GateStatusUL: 0, GateStatusDL: 0, Qfi: 0, MaxBitrateUL: 1000000, MaxBitrateDL: 100000, StartUL: 0, StartDL: 0}
+
+	if err := bpfObjects.FarMap.Put(uint32(1), unsafe.Pointer(&far)); err != nil {
+		return 0, fmt.Errorf("benchmark run failed: %v", err)
+	}
+	if err := bpfObjects.QerMap.Put(uint32(1), unsafe.Pointer(&qer)); err != nil {
+		return 0, fmt.Errorf("benchmark run failed: %v", err)
+	}
+	bpfObjects.PdrMapUplinkIp4.Put(teid, unsafe.Pointer(&pdr))
+
+	_, duration, err := bpfObjects.UpfIpEntrypointFunc.Benchmark(packet.Bytes(), repeat, func() {})
 	if err != nil {
 		return 0, fmt.Errorf("benchmark run failed: %v", err)
 	}
@@ -187,6 +302,42 @@ func TestEntrypointBenchmark(t *testing.T) {
 
 	t.Run("Arp (x1000000) benchmark", func(t *testing.T) {
 		duration, err := testArpBenchmark(bpfObjects, 1000000)
+		if err != nil {
+			t.Fatalf("test failed: %s", err)
+		}
+
+		t.Logf("%s result: %d ns", t.Name(), duration)
+	})
+
+	t.Run("Gtp (x1)) benchmark", func(t *testing.T) {
+		duration, err := testGtpBenchmark(bpfObjects, 1)
+		if err != nil {
+			t.Fatalf("test failed: %s", err)
+		}
+
+		t.Logf("%s result: %d ns", t.Name(), duration)
+	})
+
+	t.Run("Gtp (x1000000) benchmark", func(t *testing.T) {
+		duration, err := testGtpBenchmark(bpfObjects, 1000000)
+		if err != nil {
+			t.Fatalf("test failed: %s", err)
+		}
+
+		t.Logf("%s result: %d ns", t.Name(), duration)
+	})
+
+	t.Run("Gtp with PDR (x1)) benchmark", func(t *testing.T) {
+		duration, err := testGtpWithPDRBenchmark(bpfObjects, 1)
+		if err != nil {
+			t.Fatalf("test failed: %s", err)
+		}
+
+		t.Logf("%s result: %d ns", t.Name(), duration)
+	})
+
+	t.Run("Gtp with PDR (x1000000) benchmark", func(t *testing.T) {
+		duration, err := testGtpWithPDRBenchmark(bpfObjects, 1000000)
 		if err != nil {
 			t.Fatalf("test failed: %s", err)
 		}
