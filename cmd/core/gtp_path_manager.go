8ff76c2243f8c35b8db79a80c3b1484f34bf9216
pirog-spb <arozhkevich@eedgecom.ru>
2023-11-15 02:48:02 +0300

    make gtp path manager work

--- cmd/core/gtp_path_manager.go
+++ cmd/core/gtp_path_manager.go
@@ -1,34 +1,62 @@
 package core
 
 import (
+	"context"
 	"errors"
 	"fmt"
 	"net"
 	"time"
 
-	"github.com/edgecomllc/eupf/cmd/config"
 	"github.com/google/gopacket"
 	"github.com/google/gopacket/layers"
+	"github.com/rs/zerolog/log"
 )
 
 type GtpPathManager struct {
-	localAddress string
-	peers        map[uint32]uint16
+	localAddress  string
+	peers         map[string]uint16
+	checkInterval time.Duration
+	ctx           context.Context
+	cancelCtx     context.CancelFunc
 }
 
-func New(localAddress net.IP) *GtpPathManager {
-	return &GtpPathManager{}
+func NewGtpPathManager(localAddress string, interval time.Duration) *GtpPathManager {
+	ctx, cancelCtx := context.WithCancel(context.Background())
+	return &GtpPathManager{localAddress: localAddress, peers: map[string]uint16{}, checkInterval: interval, ctx: ctx, cancelCtx: cancelCtx}
+}
+
+func (gtpPathManager *GtpPathManager) AddGtpPath(gtpPeerAddress string) {
+	gtpPathManager.peers[gtpPeerAddress] = 0
 }
 
 func (gtpPathManager *GtpPathManager) Run() {
 	go func() {
+		ticker := time.NewTicker(gtpPathManager.checkInterval)
+		defer ticker.Stop()
 		for {
-			time.Sleep(time.Duration(config.Conf.HeartbeatInterval) * time.Second)
+			select {
+			case <-gtpPathManager.ctx.Done():
+				// The context is over, stop processing results
+				return
+			case <-ticker.C:
+				for peer, sequenceNumber := range gtpPathManager.peers {
+					log.Info().Msgf("Send GTP Echo to %s, seq %d", peer, sequenceNumber)
+					if err := gtpPathManager.sendEcho(peer, sequenceNumber); err != nil {
+						log.Warn().Msgf("%v", err)
+					} else {
+						gtpPathManager.peers[peer] = sequenceNumber + 1
+					}
+				}
+			}
 		}
 	}()
 }
 
-func (gtpPathManager *GtpPathManager) sendEcho(seq uint16) error {
+func (gtpPathManager *GtpPathManager) Stop() {
+	gtpPathManager.cancelCtx()
+}
+
+func (gtpPathManager *GtpPathManager) sendEcho(gtpPeerAddress string, seq uint16) error {
 	gtpEchoRequest := gopacket.NewSerializeBuffer()
 	if err := gopacket.SerializeLayers(gtpEchoRequest, gopacket.SerializeOptions{},
 		&layers.GTPv1U{
@@ -41,21 +69,28 @@ func (gtpPathManager *GtpPathManager) sendEcho(seq uint16) error {
 		return fmt.Errorf("serializing input packet failed: %v", err)
 	}
 
-	// udpAddr, err := net.ResolveUDPAddr("udp", gtpPathManager.localAddress)
-	// if err != nil {
-	// 	log.Panic().Msgf("Can't resolve local UDP address: %s", err.Error())
-	// 	return err
-	// }
-	conn, err := net.Dial("udp", "127.0.0.1:2152")
+	udpLocalAddr, err := net.ResolveUDPAddr("udp", gtpPathManager.localAddress)
 	if err != nil {
-		return fmt.Errorf("... failed: %v", err)
+		return fmt.Errorf("can't resolve local UDP address: %v", err)
+	}
+	udpRemoteAddr, err := net.ResolveUDPAddr("udp", gtpPeerAddress)
+	if err != nil {
+		return fmt.Errorf("can't resolve remote UDP address: %v", err)
+	}
+	conn, err := net.DialUDP("udp", udpLocalAddr, udpRemoteAddr)
+	if err != nil {
+		return fmt.Errorf("can't create UDP connection: %v", err)
+	}
+	defer conn.Close()
+
+	if _, err := conn.Write(gtpEchoRequest.Bytes()); err != nil {
+		return fmt.Errorf("can't send echo request: %v", err)
 	}
 
-	conn.Write(gtpEchoRequest.Bytes())
 	buf := make([]byte, 1500)
 	n, err := conn.Read(buf)
 	if err != nil {
-		return fmt.Errorf("... failed: %v", err)
+		return fmt.Errorf("can't read echo response: %v", err)
 	}
 
 	response := gopacket.NewPacket(buf[:n], layers.LayerTypeGTPv1U, gopacket.Default)
@@ -63,16 +98,16 @@ func (gtpPathManager *GtpPathManager) sendEcho(seq uint16) error {
 		gtp, _ := gtpLayer.(*layers.GTPv1U)
 
 		if gtp.MessageType != 2 { //GTPU_ECHO_RESPONSE
-			return fmt.Errorf("unexpected gtp response: %d", gtp.MessageType)
+			return fmt.Errorf("unexpected gtp echo response: %d", gtp.MessageType)
 		}
 		if gtp.SequenceNumber != seq {
-			return fmt.Errorf("unexpected gtp sequence: %d", gtp.SequenceNumber)
+			return fmt.Errorf("unexpected gtp echo response sequence: %d", gtp.SequenceNumber)
 		}
 		if gtp.TEID != 0 {
-			return fmt.Errorf("unexpected gtp TEID: %d", gtp.TEID)
+			return fmt.Errorf("unexpected gtp echo response TEID: %d", gtp.TEID)
 		}
 	} else {
-		return errors.New("unexpected response")
+		return errors.New("unexpected gtp echo response")
 	}
 
 	return nil
