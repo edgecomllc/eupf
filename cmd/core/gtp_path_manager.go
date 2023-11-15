e90451990e41854942d11f314fec812409f8db1f
pirog-spb <arozhkevich@eedgecom.ru>
2023-11-15 22:17:26 +0300

    add duration output to trace

--- cmd/core/gtp_path_manager.go
+++ cmd/core/gtp_path_manager.go
@@ -40,12 +40,16 @@ func (gtpPathManager *GtpPathManager) Run() {
 				return
 			case <-ticker.C:
 				for peer, sequenceNumber := range gtpPathManager.peers {
-					log.Trace().Msgf("Send GTP Echo to %s, seq %d", peer, sequenceNumber)
-					if err := gtpPathManager.sendEcho(peer, sequenceNumber); err != nil {
+					log.Trace().Msgf("Send GTP Echo request to %s, seq %d", peer, sequenceNumber)
+					elapseTime, err := gtpPathManager.sendEcho(peer, sequenceNumber)
+					if err != nil {
 						log.Warn().Msgf("%v", err)
-					} else {
-						gtpPathManager.peers[peer] = sequenceNumber + 1
+						continue
 					}
+
+					log.Trace().Msgf("Received GTP Echo response from %s, seq %d in %d ms", peer, sequenceNumber, elapseTime.Milliseconds())
+					gtpPathManager.peers[peer] = sequenceNumber + 1
+
 				}
 			}
 		}
@@ -56,7 +60,7 @@ func (gtpPathManager *GtpPathManager) Stop() {
 	gtpPathManager.cancelCtx()
 }
 
-func (gtpPathManager *GtpPathManager) sendEcho(gtpPeerAddress string, seq uint16) error {
+func (gtpPathManager *GtpPathManager) sendEcho(gtpPeerAddress string, seq uint16) (time.Duration, error) {
 	gtpEchoRequest := gopacket.NewSerializeBuffer()
 	if err := gopacket.SerializeLayers(gtpEchoRequest, gopacket.SerializeOptions{},
 		&layers.GTPv1U{
@@ -66,50 +70,53 @@ func (gtpPathManager *GtpPathManager) sendEcho(gtpPeerAddress string, seq uint16
 			SequenceNumber: seq,
 		},
 	); err != nil {
-		return fmt.Errorf("serializing input packet failed: %v", err)
+		return 0, fmt.Errorf("serializing input packet failed: %v", err)
 	}
 
 	udpLocalAddr, err := net.ResolveUDPAddr("udp", gtpPathManager.localAddress)
 	if err != nil {
-		return fmt.Errorf("can't resolve local UDP address: %v", err)
+		return 0, fmt.Errorf("can't resolve local UDP address: %v", err)
 	}
 	udpRemoteAddr, err := net.ResolveUDPAddr("udp", gtpPeerAddress)
 	if err != nil {
-		return fmt.Errorf("can't resolve remote UDP address: %v", err)
+		return 0, fmt.Errorf("can't resolve remote UDP address: %v", err)
 	}
 	conn, err := net.DialUDP("udp", udpLocalAddr, udpRemoteAddr)
 	if err != nil {
-		return fmt.Errorf("can't create UDP connection: %v", err)
+		return 0, fmt.Errorf("can't create UDP connection: %v", err)
 	}
 	defer conn.Close()
 
-	if _, err := conn.Write(gtpEchoRequest.Bytes()); err != nil {
-		return fmt.Errorf("can't send echo request: %v", err)
-	}
-
-	buf := make([]byte, 1500)
+	receiveBuffer := make([]byte, 1500)
 	conn.SetReadDeadline(time.Now().Add(time.Second * 3))
-	n, err := conn.Read(buf)
-	if err != nil {
-		return fmt.Errorf("can't read echo response: %v", err)
+
+	sendTime := time.Now()
+	if _, err := conn.Write(gtpEchoRequest.Bytes()); err != nil {
+		return 0, fmt.Errorf("can't send echo request: %v", err)
 	}
 
-	response := gopacket.NewPacket(buf[:n], layers.LayerTypeGTPv1U, gopacket.Default)
+	n, err := conn.Read(receiveBuffer)
+	if err != nil {
+		return 0, fmt.Errorf("can't read echo response: %v", err)
+	}
+	elapsedTime := time.Since(sendTime)
+
+	response := gopacket.NewPacket(receiveBuffer[:n], layers.LayerTypeGTPv1U, gopacket.Default)
 	if gtpLayer := response.Layer(layers.LayerTypeGTPv1U); gtpLayer != nil {
 		gtp, _ := gtpLayer.(*layers.GTPv1U)
 
 		if gtp.MessageType != 2 { //GTPU_ECHO_RESPONSE
-			return fmt.Errorf("unexpected gtp echo response: %d", gtp.MessageType)
+			return 0, fmt.Errorf("unexpected gtp echo response: %d", gtp.MessageType)
 		}
 		if gtp.SequenceNumber != seq {
-			return fmt.Errorf("unexpected gtp echo response sequence: %d", gtp.SequenceNumber)
+			return 0, fmt.Errorf("unexpected gtp echo response sequence: %d", gtp.SequenceNumber)
 		}
 		if gtp.TEID != 0 {
-			return fmt.Errorf("unexpected gtp echo response TEID: %d", gtp.TEID)
+			return 0, fmt.Errorf("unexpected gtp echo response TEID: %d", gtp.TEID)
 		}
 	} else {
-		return errors.New("unexpected gtp echo response")
+		return 0, errors.New("unexpected gtp echo response")
 	}
 
-	return nil
+	return elapsedTime, nil
 }
