09996637e6dd5c3f269f1f1fa0a98b405cc4e29b
artem <artem@pingocean.com>
2023-11-24 01:35:57 +0600

    improvements based on comments

--- cmd/core/service/resource_manager.go
+++ cmd/core/service/resource_manager.go
@@ -102,15 +102,3 @@ func (fteidm *FTEIDM) ReleaseTEID(seID uint64) {
 		delete(fteidm.busyTEIDs, seID)
 	}
 }
-
-func nextIP(ip net.IP) net.IP {
-	nextIP := make(net.IP, len(ip))
-	copy(nextIP, ip)
-	for i := len(nextIP) - 1; i >= 0; i-- {
-		nextIP[i]++
-		if nextIP[i] > 0 {
-			break
-		}
-	}
-	return nextIP
-}
