52e843d6dfab5ff0463ca466c14293ad3ca45f40
PapaySail <125686203+PapaySail@users.noreply.github.com>
2023-12-07 14:01:10 +0500

    fix wrong `dev eth1` in `ip route add` command

--- docs/deployments/oai-nfapi-sim-compose/eupf/entrypoint.sh
+++ docs/deployments/oai-nfapi-sim-compose/eupf/entrypoint.sh
@@ -17,7 +17,7 @@ echo "1200 n6if" >> /etc/iproute2/rt_tables
 ip rule add from 10.60.0.0/16 table n6if
 ip rule add from 12.1.1.0/24 table n6if
 #ip route add default via `nslookup nat.free5gc.org | awk '/^Address: / { print $2 }'` dev eth0 table n6if
-ip route add default via `nslookup natn6.free5gc.org | awk '/^Address: / { print $2 }'` dev eth1 table n6if
+ip route add default via `nslookup natn6.free5gc.org | awk '/^Address: / { print $2 }'` table n6if
 
 # Run app replacing current shell (to preserve signal handling) and forward cmd arguments
 exec /app/bin/eupf "$@"
