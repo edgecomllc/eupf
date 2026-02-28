c4a868c4d7d8a4a8995bc65964f9ed64c0b026c8
pirog-spb <a.rozhkevich@edgecom.ru>
2026-02-28 22:19:22 +0300

    fix build
    Signed-off-by: pirog-spb <a.rozhkevich@edgecom.ru>

--- cmd/ebpf/xdp/n3n6_entrypoint.c
+++ cmd/ebpf/xdp/n3n6_entrypoint.c
@@ -607,8 +607,7 @@ int upf_ip_entrypoint_func(struct xdp_md *ctx) {
         .xdp_ctx = ctx,
         .counters = &statistic->upf_counters,
         .n3_n6_counter = &statistic->upf_n3_n6_counter,
-        .trace = 0,
-        .recursion = 0};
+        .trace = 0};
 
     enum xdp_action action = process_packet(&context);
     statistic->xdp_actions[action & EUPF_MAX_XDP_ACTION_MASK] += 1;
